9694ceebd205ebc6adb575c59281568b72711842
Gusttavo Ruy <7leckler@gmail.com>
2024-08-07 12:59:19 -0300

    feat: setup

---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MyFirstMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void aumentar(object sender, EventArgs e)
+        {
+            count++;
+
+
+            if (count == 1)
+                ClicksLabel.Text = $"Clicks: {count}";
+            else
+                ClicksLabel.Text = $"Clicks: {count}";
+
+            SemanticScreenReader.Announce(ClicksLabel.Text);
+        }
+        private void diminuir(object sender, EventArgs e) {
+            count--;
+
+            if (count == 1)
+                ClicksLabel.Text = $"Clicks: {count}";
+            else
+                ClicksLabel.Text = $"Clicks: {count}";
+        }
+
+        private void goToPageOne(object sender, EventArgs e) {
+            Navigation.PushAsync(new NewPage1());
+        }
+    }
+
+}
